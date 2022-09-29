65648138a0722f9d08d805a75304661b8ca4ba27
Samuel Park <sd99park@gmail.com>
2022-09-29 16:17:01 -0500

    Made profiles and header responsive, work on mobil

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,119 @@
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import rainbow from "../img/Rainbow.png";
+import Hamburger from "../img/Hamburger.png";
 import "./Header.css";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [windowDimension, setWindowDimension] = useState(null);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimension(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isNotMobile = windowDimension > 768;
 
   return (
     <>
-      <div className="head-container">
-        <img
-          src={rainbow}
-          onClick={() => navigate("/")}
-          alt="Rainbow"
-          style={{ height: "100px" }}
-        />
+      {isNotMobile ? (
+        <>
+          <div className="head-container">
+            <img
+              src={rainbow}
+              onClick={() => navigate("/")}
+              alt="Rainbow"
+              style={{ height: "100px" }}
+            />
 
-        <h2 onClick={() => navigate("/About")} className="links">
-          About
-        </h2>
-        <h2 onClick={() => navigate("/Registration")} className="links">
-          Class Registration
-        </h2>
-        <h2 onClick={() => navigate("/Contact")} className="links">
-          Contact Us
-        </h2>
-        <h2 onClick={() => navigate("/Donate")} className="links">
-          Donate
-        </h2>
-      </div>
+            <h2 onClick={() => navigate("/About")} className="links">
+              About
+            </h2>
+            <h2 onClick={() => navigate("/Registration")} className="links">
+              Class Registration
+            </h2>
+            <h2 onClick={() => navigate("/Contact")} className="links">
+              Contact Us
+            </h2>
+            <h2 onClick={() => navigate("/Donate")} className="links">
+              Donate
+            </h2>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="head-container-mobile">
+            <img
+              src={rainbow}
+              onClick={() => navigate("/")}
+              alt="Rainbow"
+              style={{ height: "100px" }}
+            />
+            <img
+              src={Hamburger}
+              onClick={() => {
+                if (menuIsOpen) {
+                  setMenuIsOpen(false);
+                } else {
+                  setMenuIsOpen(true);
+                }
+              }}
+              alt="Rainbow"
+              className="hamburger"
+            />
+            {menuIsOpen && (
+              <div>
+                <h2
+                  onClick={() => {
+                    setMenuIsOpen(false);
+                    navigate("/About");
+                  }}
+                  className="mobile-links"
+                >
+                  About
+                </h2>
+                <h2
+                  onClick={() => {
+                    setMenuIsOpen(false);
+                    navigate("/Registration");
+                  }}
+                  className="mobile-links"
+                >
+                  Class Registration
+                </h2>
+                <h2
+                  onClick={() => {
+                    setMenuIsOpen(false);
+                    navigate("/Contact");
+                  }}
+                  className="mobile-links"
+                >
+                  Contact Us
+                </h2>
+                <h2
+                  onClick={() => {
+                    setMenuIsOpen(false);
+                    navigate("/Donate");
+                  }}
+                  className="mobile-links"
+                >
+                  Donate
+                </h2>
+              </div>
+            )}
+          </div>
+        </>
+      )}
     </>
   );
 };
--- src/components/Register.js
+++ src/components/Register.js
@@ -9,9 +9,9 @@ const Register = () => {
         src="https://docs.google.com/forms/d/e/1FAIpQLSebE_qNM1EAqBdcx1Ab73jI3bQUGagnNwnH7wF0ZOdjdKjxoA/viewform?embedded=true"
         width="100%"
         height="1600"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
         title="Registration"
       >
         Loading…
