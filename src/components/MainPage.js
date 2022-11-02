d651063b5ecc22cc67cda758048b2384c17f00b4
Samuel Park <sd99park@gmail.com>
2022-11-02 16:32:37 -0500

    Additions to the main page

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,23 +1,68 @@
 import handShake from "../img/shakeHands.jpg";
+import EngageChurchPic from "../img/EngageChurchPic.png";
+import ltrLogo from "../img/ltrLogo.png";
+import stratPartPic from "../img/StratPartPic.png";
+import { useNavigate } from "react-router-dom";
+import * as constants from "../constants/MainPageConstants";
 import { LoremIpsum } from "react-lorem-ipsum";
 import "./MainPage.css";
 
 const MainPage = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div className="Container32">
-        <img src={handShake} alt="HandShake" style={{ width: "100%" }} />
-        <div className="textbox1">
-          <h3 style={{ margin: 0 }}>Some sort of title</h3>
-          <LoremIpsum />
+      <div className="main-page-container">
+        <div className="image-container">
+          <img
+            src={ltrLogo}
+            alt="HandShake"
+            style={{ width: "65%", border: "solid" }}
+          />
         </div>
 
-        <h1>Main page</h1>
-        <h1>Main page</h1>
-        <h1>Main page</h1>
-        <h1>Main page</h1>
-        <h1>Main page</h1>
-        <h1>Main page</h1>
+        <div className="textbox1">
+          <h2>{constants.missionStatement}</h2>
+        </div>
+        <div className="main-card-container">
+          <div className="main-card">
+            <img src={EngageChurchPic} className="card1-img" />
+            <h3>{constants.engageTitle}</h3>
+            <p className="main-card-text">{constants.engageText}</p>
+            <button className="main-section-button">Do something</button>
+          </div>
+          <div className="main-card">
+            <img src={stratPartPic} className="card2-img" />
+            <h3>{constants.partnerTitle}</h3>
+            <p className="main-card-text">{constants.partnerText}</p>
+            <button className="main-section-button">Do something</button>
+          </div>
+
+          <div className="main-card">
+            <img src={ltrLogo} className="card3-img" />
+            <h3>{constants.primaryTitle}</h3>
+            <p className="main-card-text">{constants.primaryText}</p>
+            <button
+              className="main-section-button"
+              onClick={() => navigate("/Registration")}
+            >
+              Sign Up!
+            </button>
+          </div>
+        </div>
+
+        <div className="textbox2">
+          <h3>{constants.whatWeDoTitle}</h3>
+          <ul>
+            <li>{constants.whatWeDo1}</li>
+            <li>{constants.whatWeDo2}</li>
+            <li>{constants.whatWeDo3}</li>
+            <li>{constants.whatWeDo4}</li>
+            <li>{constants.whatWeDo5}</li>
+            <li>{constants.whatWeDo6}</li>
+            <li>{constants.whatWeDo7}</li>
+          </ul>
+        </div>
       </div>
     </>
   );
---
+++ src/constants/MainPageConstants.js
@@ -0,0 +1,39 @@
+export const missionStatement =
+  //   "LTR Ministries exists to educate and encourage white Christians to recognize, engage, and advocate for their neighbor.";
+  "Educating and encouraging white Christians to recognize, engage, and advocate for their neighbor.";
+
+export const engageTitle = "Engaging Churches";
+
+export const engageText =
+  "We accomplish this by engaging with predominantly white churches and offering resources and guidance as they navigate the journey of racial unity and reconciliation.";
+
+export const partnerTitle = "Strategic Partners";
+
+export const partnerText =
+  "We also partner with organizations who are in the work of racial reconciliation in the church.";
+
+export const primaryTitle = "Primary Resource";
+
+export const primaryText =
+  "Our primary resource is the 5-week course called Let’s Talk Race: a Beginner’s Guide to Conversations about Race (LTR), which can be hosted in person or virtually. We are open and excited about finding and creating new opportunities to help meet our goal and vision.";
+
+export const whatWeDoTitle = "What We Do:";
+
+export const whatWeDo1 =
+  "Conduct Let’s Talk Race: a Beginner’s Guide to Conversations about Race (LTR) classes";
+
+export const whatWeDo2 = "Train and support LTR facilitators";
+
+export const whatWeDo3 = "Keep LTR material up to date and current";
+
+export const whatWeDo4 =
+  "Partner with churches to train members as facilitators of LTR classes";
+
+export const whatWeDo5 =
+  "Help church leadership in predominantly white churches develop a sustainable strategy for racial reconciliation. This can include facilitating the creation of lay-led ministry groups to help lead the church in areas and activities of reconciliation.";
+
+export const whatWeDo6 =
+  "Create a network of these ministry groups who can help, encourage and inspire each other toward greater work in the area of racial reconciliation through sharing ideas, successes, failures, etc.";
+
+export const whatWeDo7 =
+  "Develop other resources as necessary to achieve the mission";
