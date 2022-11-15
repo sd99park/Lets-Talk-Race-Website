d7242eedb4bf8db76c5fa824e1860ff72aac9b36
Samuel Park <sd99park@gmail.com>
2022-11-14 19:06:21 -0600

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import ltrLogo from "../img/ltrLogo.png";
+import BannerLogo from "../img/BannerLogo.png";
 import "./Footer.css";
 
 const Footer = () => {
@@ -8,10 +8,42 @@ const Footer = () => {
   return (
     <>
       <div className="footer-container">
-        <img src={ltrLogo} alt="ltrLogo" />
-        <p onClick={() => navigate("/")}>Main</p>
-        <p onClick={() => navigate("/About")}>About Us</p>
-        <p>Email: ~~~~~~~</p>
+        <img
+          className="foot-logo"
+          src={BannerLogo}
+          alt="ltrLogo"
+          onClick={() => navigate("/")}
+        />
+
+        <div className="foot-all-links">
+          <p className="foot-links" onClick={() => navigate("/About")}>
+            About Us
+          </p>
+          <p className="foot-links" onClick={() => navigate("/Registration")}>
+            Sign up for a class
+          </p>
+          <p className="foot-links" onClick={() => navigate("/Contact")}>
+            Contact Us
+          </p>
+        </div>
+
+        <a
+          className="foot-links"
+          href="https://gmail.us7.list-manage.com/subscribe?u=d4e916972ae8c52a294d25f57&id=1bc7641333"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Join our mailing list
+        </a>
+
+        <div className="foot-button-div">
+          <button
+            className="foot-donate-button"
+            onClick={() => navigate("/Donate")}
+          >
+            Donate
+          </button>
+        </div>
       </div>
     </>
   );
