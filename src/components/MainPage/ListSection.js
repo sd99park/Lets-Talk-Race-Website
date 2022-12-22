c291b89eba3b2d6f41053838d685fbb194abad82
Samuel Park <sd99park@gmail.com>
2022-12-21 23:47:10 -0600

    Made listSection pretty

--- src/components/MainPage/ListSection.js
+++ src/components/MainPage/ListSection.js
@@ -6,17 +6,23 @@ import "./ListSection.css";
 const ListSection = () => {
   return (
     <div className="main-page-list">
-      <img src={diffPeople} alt="diffPeople" style={{ width: "30%" }} />
-      <div>
+      <div className="list-section-img">
+        <img
+          src={diffPeople}
+          alt="diffPeople"
+          style={{ maxWidth: "40%", minWidth: "20rem" }}
+        />
+      </div>
+      <div className="list-section-list">
         <h3>{constants.whatWeDoTitle}</h3>
         <ul>
-          <li>{constants.whatWeDo1}</li>
-          <li>{constants.whatWeDo2}</li>
-          <li>{constants.whatWeDo3}</li>
-          <li>{constants.whatWeDo4}</li>
-          <li>{constants.whatWeDo5}</li>
-          <li>{constants.whatWeDo6}</li>
-          <li>{constants.whatWeDo7}</li>
+          <li className="list-section-bullet">{constants.whatWeDo1}</li>
+          <li className="list-section-bullet">{constants.whatWeDo2}</li>
+          <li className="list-section-bullet">{constants.whatWeDo3}</li>
+          <li className="list-section-bullet">{constants.whatWeDo4}</li>
+          <li className="list-section-bullet">{constants.whatWeDo5}</li>
+          <li className="list-section-bullet">{constants.whatWeDo6}</li>
+          <li className="list-section-bullet">{constants.whatWeDo7}</li>
         </ul>
       </div>
     </div>
