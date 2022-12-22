c3974b7c8b1cf7c016689ec97b013f5e25c840a7
Samuel Park <sd99park@gmail.com>
2022-12-22 11:29:46 -0600

    Made story pretty

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,30 @@
+import "./About.css";
 import ProfilePage from "./Profiles/ProfilePage";
+import Story from "./Story";
+import headBanner from "../../img/headbanner.png";
+import heartBanner from "../../img/heartbanner.png";
 import { useState } from "react";
 
 const About = () => {
   return (
     <>
-      <h1> This is the about page</h1>
-      <h1> This is the about page</h1>
-      <h1> This is the about page</h1>
-      <h1> This is the about page</h1>
-
-      <ProfilePage />
+      <img src={headBanner} style={{ width: "100%" }} />
+      <div className="about-story">
+        <Story />
+      </div>
+      <div className="about-verse">
+        <div>
+          <p className="about-verse-text">
+            "Do nothing out of selfish ambition or vain conceit. Rather, in
+            humility value others above yourselves, not looking to your own
+            interests but each of you to the interests of the others."
+            <p className="about-verse-verse">Phil 2: 3-4</p>
+          </p>
+        </div>
+      </div>
+      <div className="about-profile">
+        <ProfilePage />
+      </div>
     </>
   );
 };
---
+++ src/components/About/Story.js
@@ -0,0 +1,79 @@
+import "./Story.css";
+import unity from "../../img/unity.png";
+import hands from "../../img/hands.png";
+
+const Story = () => {
+  return (
+    <>
+      <div className="story-container">
+        <h1 className="story-header">Our Story</h1>
+        <div className="story-one">
+          <div className="story-one-txt">
+            <p className="story-p">
+              The first iteration of Let's Talk Race: a Beginner's Guide to
+              Conversations About Race was a 4 week class created by Ron Holland
+              at The Hills church in Ft. Worth, Texas in the summer of 2018.
+              “Let's Get Ready to Talk About Race” was later promoted by the
+              church following their pastor's six week sermon called “Why Talk
+              About Race?” in January of 2019. It was intended to prepare White
+              people to be able to have meaningful conversations about race in a
+              multiracial/multiethnic environment. Ron recognized that there was
+              a lot of history and a lot of vocabulary that White people just
+              didn't know.
+            </p>
+            <p className="story-p">
+              In the fall of 2019, Shelley Park, Ron's daughter, felt an urgent
+              call from the Lord to “do something.” She had seen many BIPOC in
+              the racial reconciliation space ask white people to teach other
+              white people. She remembered her dad's course and thought, “this
+              is something I can do.” Shelley took the presentations that Ron
+              had created and “made them her own.” In February 2020 she invited
+              a few friends to be her guinea pigs. These four friends gathered
+              around her dining table each week and walked through the classwork
+              together. A second group was scheduled to start in March 2020 and
+              then Covid hit.
+            </p>
+          </div>
+          <div className="story-one-img">
+            <img src={hands} style={{ width: "30rem" }} />
+          </div>
+        </div>
+
+        <div className="story-two">
+          <div className="story-two-img">
+            <img src={unity} style={{ width: "25rem" }} />
+          </div>
+          <div className="story-two-txt">
+            <p className="story-p">
+              In late April, Shelley wondered if the class might be taught on
+              Zoom, so she reached back out to the second group of friends and
+              they all agreed to be another set of guinea pigs. They met for
+              four weeks on Zoom working through the recently renamed “Let's
+              Talk Race.” It was during this class, that George Floyd was
+              murdered in Minneapolis.
+            </p>
+            <p className="story-p">
+              The summer of 2020 was a moment in history that will be studied
+              for years to come. The initial reaction from White people was one
+              of a genuine desire to learn and understand. Let's Talk Race was
+              the perfect answer for many people. After adding a fifth week to
+              help accommodate more discussion, Shelley, sometimes joined by her
+              husband, David, took more than 300 folks through the class over
+              the next 18 months. The class was never advertised, only spread
+              via word of mouth and participants came from all over the United
+              States. Ron Holland continued to host classes at his church and
+              also took several church leadership groups through the material.
+            </p>
+            <p className="story-p">
+              In the fall of 2022, Shelley & David formed LTR Ministries, a
+              501(c)3 not for profit organization aimed at helping white people
+              begin the journey of racial reconciliation.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Story;
