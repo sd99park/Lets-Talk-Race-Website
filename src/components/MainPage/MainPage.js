2dd6fbad47f75ea1a2a918f001b2b50e11efbf2e
Samuel Park <sd99park@gmail.com>
2022-12-21 23:48:02 -0600

    Bible verses pt 2

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -10,7 +10,26 @@ const MainPage = () => {
     <>
       <div className="main-page-container">
         <OpeningPage />
+        <div className="main-verse-one">
+          <div>
+            <p className="main-verse-text">
+              “After this I looked, and there before me was a great multitude
+              that no one could count, from every nation, tribe, people and
+              language, standing before the throne and before the Lamb. They
+              were wearing white robes and were holding palm branches in their
+              hands.”
+              <p className="main-verse-verse">Rev. 7:9</p>
+            </p>
+          </div>
+        </div>
         <MainCards />
+        <div className="main-verse-two">
+          <p className="main-verse-text">
+            “You are in me and I am in you. I pray that they also can be one in
+            us. THEN THE WORLD WILL BELIEVE THAT YOU SENT ME.”
+            <p className="main-verse-verse">John 17:21</p>
+          </p>
+        </div>
         <ListSection />
       </div>
     </>
