a40ef707b5a8601a48749ae8e7ce3ad34d15ec77
Samuel Park <sd99park@gmail.com>
2022-12-21 23:46:53 -0600

    Made footer dynamic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,18 @@ import "./Footer.css";
 const Footer = () => {
   const navigate = useNavigate();
 
+  const openDonate = () => {
+    window.open(
+      "https://crm.bloomerang.co/HostedDonation?ApiKey=pub_85ea1511-7ae8-11ed-9ee6-0606629de7e9&WidgetId=45056"
+    );
+  };
+
+  const openMailList = () => {
+    window.open(
+      "https://gmail.us7.list-manage.com/subscribe?u=d4e916972ae8c52a294d25f57&id=1bc7641333"
+    );
+  };
+
   return (
     <>
       <div className="footer-container">
@@ -15,35 +27,24 @@ const Footer = () => {
           onClick={() => navigate("/")}
         />
 
-        <div className="foot-all-links">
-          <p className="foot-links" onClick={() => navigate("/About")}>
-            About Us
-          </p>
-          <p className="foot-links" onClick={() => navigate("/Registration")}>
-            Sign up for a class
-          </p>
-          <p className="foot-links" onClick={() => navigate("/Contact")}>
-            Contact Us
-          </p>
-        </div>
-
-        <a
-          className="foot-links"
-          href="https://gmail.us7.list-manage.com/subscribe?u=d4e916972ae8c52a294d25f57&id=1bc7641333"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Join our mailing list
-        </a>
-
         <div className="foot-button-div">
-          <button
-            className="foot-donate-button"
-            onClick={() => navigate("/Donate")}
-          >
+          <button className="foot-donate-button" onClick={() => openDonate()}>
             Donate
           </button>
         </div>
+        <p className="foot-links" onClick={() => navigate("/About")}>
+          About Us
+        </p>
+        <p className="foot-links" onClick={() => navigate("/Registration")}>
+          Sign up for a class
+        </p>
+        <p className="foot-links" onClick={() => navigate("/Contact")}>
+          Contact Us
+        </p>
+
+        <p className="foot-links" onClick={() => openMailList()}>
+          Join our mailing list
+        </p>
       </div>
     </>
   );
