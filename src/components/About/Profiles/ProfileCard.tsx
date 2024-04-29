7954f1e54a9f93fe4adf9a45025299f7e6c95b2d
sd99park <sd99park@gmail.com>
2024-04-28 20:32:15 -0500

    Profile Cards

---
+++ src/components/About/Profiles/ProfileCard.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+interface ProfileCardProps {
+  bio1: string;
+  bio2?: string;
+  bio3?: string;
+  bio4?: string;
+  name: string;
+  title: string;
+  image: string;
+  summary: string;
+}
+
+const ProfileCard: React.FC<ProfileCardProps> = ({
+  bio1,
+  bio2,
+  bio3,
+  bio4,
+  name,
+  title,
+  image,
+  summary,
+}) => {
+  return (
+    <>
+      <div className="card w-96 bg-gray-50 shadow-xl m-10 font-montserrat z-0">
+        <figure className="px-10 pt-10">
+          <img src={image} alt="Headshot" className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{name}</h2>
+          <p className="text-2xl">{title}</p>
+          <p>{summary}</p>
+          <div className="card-actions">
+            <button
+              className="btn btn-primary"
+              onClick={() =>
+                (
+                  document.getElementById(
+                    `my_modal_${name}`
+                  ) as HTMLDialogElement
+                ).showModal()
+              }
+            >
+              Read more
+            </button>
+            <dialog id={`my_modal_${name}`} className="modal">
+              <div className="modal-box w-11/12 max-w-5xl">
+                <div className="flex">
+                  <figure className="px-10 pt-10">
+                    <img src={image} alt="Headshot" className="rounded-xl" />
+                  </figure>
+                  <div className="flex items-center justify-center">
+                    <div>
+                      <h2 className="card-title">{name}</h2>
+                      <p className="text-2xl">{title}</p>
+                    </div>
+                  </div>
+                </div>
+                <p className="py-2">{bio1}</p>
+                <p className="py-2">{bio2}</p>
+                <p className="py-2">{bio3}</p>
+                <p className="py-2">{bio4}</p>
+
+                <div className="modal-action">
+                  <form method="dialog">
+                    <button className="btn">Close</button>
+                  </form>
+                </div>
+              </div>
+            </dialog>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProfileCard;
